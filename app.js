22bc39590ce2865954a5b64fbf0b449e774986e2
ssup <sahabatstartup@gmail.com>
2021-07-30 20:39:12 +0700

    third

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var http = require('http').Server(app);
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const passport = require('passport');
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const passport = require("passport")
 
 const{forwardAuth}=require('../config/auth');
